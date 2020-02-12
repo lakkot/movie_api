d5127fe4d65f745acbf5a0c40936949344187a43
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-12 10:40:04 +0100

    adding morgan, testing code

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+/*
+
 const os = require('os')
 console.log('type: ' + os.type());
 
@@ -7,3 +9,21 @@ fs.readFile('./file.txt', 'utf-8', (err, data) => {
   if (err) {throw err;}
   console.log('data: ' + data);
 });
+
+
+*/
+const express = require('express'),
+  morgan = require('morgan');
+
+const app = express();
+
+app.use(morgan('common'));
+
+app.get('/', function (req, res) {
+  res.send('Welcome to my app!');
+});
+app.get('/secreturl', function (req, res) {
+  res.send('This is a secret url with super top-secret content.');
+});
+
+app.listen(8080);
