c42b0051d09a052642bcf7d893c90ce0217711cc
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-19 19:02:42 +0100

    Update models.js

--- models.js
+++ models.js
@@ -17,21 +17,22 @@ var movieSchema = mongoose.Schema({
   featured : Boolean
 });
 
-var userSchema = ({
+var userSchema = mongoose.Schema({
   username : {type: String, required: true},
-  Password : {type: String, required: true},
+  password : {type: String, required: true},
   email : {type: String, required: true},
   birthday : Date,
   favMovies : [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
-/*
+
 userSchema.statics.hashPassword = function(password) {
   return bcrypt.hashSync(password, 10);
 };
+
 userSchema.statics.validatePassword = function(password) {
   return bcrypt.compareSync(password, this.password);
 };
-*/
+
 var Movie = mongoose.model('Movie', movieSchema);
 var User = mongoose.model('User', userSchema);
 
