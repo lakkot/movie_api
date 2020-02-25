4d01a169a66a983bfeb438ba1e5ead84cf7e7fae
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-25 09:58:42 +0100

    update

--- client/dist/src.78399e21.js
+++ client/dist/src.78399e21.js
@@ -31887,7 +31887,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "58816" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60735" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
