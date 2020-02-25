1d9e940354e6101fc2eeff1dc8fbdfa87c2ea59c
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-25 12:18:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -27,7 +27,20 @@ const app = express();
 app.use(bodyParser.json());
 //importing authentication file into the project
 var auth = require('./auth')(app); //this needs to be put ALWAYS after app.use(bodyParser.json());
-app.use(cors());
+
+var allowedOrigins = ['http://localhost:1234'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    if (!origin) return callback(null, true);
+    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+      var message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+      return callback(new Error(message), false);
+    }
+    return callback(null, true);
+  }
+}));
+
 
 /*****middleware functions*****/
 //reroute requests for static pages to public folder
