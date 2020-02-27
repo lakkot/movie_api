dfc536cbaaa8663706bdb67cca66331d4370eff5
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-27 17:05:44 +0100

    Update main-view.jsx

--- client/src/components/main-view/main-view.jsx
+++ client/src/components/main-view/main-view.jsx
@@ -45,12 +45,27 @@ export class MainView extends React.Component {
   onLoggedIn(authData) {
     console.log(authData);
     this.setState({
-      user: authData.username
+      user: authData.user.username
     });
 
     localStorage.setItem('token', authData.token);
     localStorage.setItem('user', authData.user.username);
     this.getMovies(authData.token);
+  }
+
+  getMovies(token) {
+    axios.get('https://mymovies-database.herokuapp.com/movies', {
+      headers: { Authorization: `Bearer ${token}` }
+    })
+      .then(response => {
+        // Assign the result to the state
+        this.setState({
+          movies: response.data
+        });
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 
   isRegistered = () => {
