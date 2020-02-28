041eb3e2b1fa85e0f3a8325c4d6f339f39a9d985
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-28 15:48:05 +0100

    Create director-view.jsx

---
+++ client/src/components/director-view/director-view.jsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Button from 'react-bootstrap/Button';
+
+import { Link } from "react-router-dom";
+
+
+import './director-view.scss'
+
+
+export class DirectorView extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+
+  render() {
+    const { director } = this.props;
+
+    if (!director) return null;
+
+
+
+
+    return (
+
+      <Container className="director-container col-6">
+        <Row>
+          <Row className="button-pane">
+            <Link to={'/'}>
+              <Button variant="secondary" type="button" className="back-button">go back</Button>
+            </Link>
+          </Row>
+
+          <Row className="director-name">
+            <h1 className="value">{director.name}</h1>
+          </Row>
+          <Row className="bio">
+            <p className="view-label label">About: </p>
+            <p className="value description"> {director.bio}</p>
+          </Row>
+
+          <Row className="director-row">
+            <p className="view-label label">Birth: </p>
+            <p className="value">{director.birth}</p>
+          </Row>
+          <Row className="director-row">
+            <p className="view-label label">Death: </p>
+            <p className="value">{director.death}</p>
+          </Row>
+
+
+        </Row>
+      </Container>
+    );
+  }
+}
+/*
+Director.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imageURL: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string
+    }),
+  }).isRequired
+};
+*/
