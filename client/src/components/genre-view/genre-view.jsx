a8ae150dd190367c29bdc62a4ceeeac4ee44d243
lakkot <40659615+lakkot@users.noreply.github.com>
2020-02-28 15:48:11 +0100

    Create genre-view.jsx

---
+++ client/src/components/genre-view/genre-view.jsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Button from 'react-bootstrap/Button';
+
+import { Link } from "react-router-dom";
+
+
+import './genre-view.scss'
+
+
+export class GenreView extends React.Component {
+  constructor() {
+    super();
+    this.state = {}
+  }
+
+
+  render() {
+    const { genre } = this.props;
+
+    if (!genre) return null;
+
+
+
+
+    return (
+
+      <Container className="genre-container col-6">
+        <Row>
+          <Row className="button-pane">
+            <Link to={'/'}>
+              <Button variant="secondary" type="button" className="back-button">go back</Button>
+            </Link>
+          </Row>
+
+          <Row className="genre-name">
+            <h1 className="value">{genre.name}</h1>
+          </Row>
+          <Row className="genre-description">
+            <p className="view-label label">Description: </p>
+            <p className="value description"> {genre.description}</p>
+          </Row>
+        </Row>
+      </Container>
+    );
+  }
+}
+/*
+Director.propTypes = {
+  movie: PropTypes.shape({
+    title: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    imageURL: PropTypes.string.isRequired,
+    genre: PropTypes.shape({
+      name: PropTypes.string
+    }),
+    director: PropTypes.shape({
+      name: PropTypes.string
+    }),
+  }).isRequired
+};
+*/
