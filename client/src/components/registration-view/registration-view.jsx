32d2be3a74959a270183ad43278a60c1714ebb76
lakkot <40659615+lakkot@users.noreply.github.com>
2020-03-02 21:09:45 +0100

    Update registration-view.jsx

--- client/src/components/registration-view/registration-view.jsx
+++ client/src/components/registration-view/registration-view.jsx
@@ -5,13 +5,17 @@ import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import './registration-view.scss'
 
+import { Link } from "react-router-dom";
+
+
 export function RegistrationView(props) {
   const [username, createUsername] = useState('');
   const [password, createPassword] = useState('');
   const [email, createEmail] = useState('');
   const [birthday, createBirthday] = useState('');
 
-
+  
+  
 
   const handleSubmit = (e) => {
     axios.post('https://mymovies-database.herokuapp.com/users', {
@@ -35,32 +39,33 @@ export function RegistrationView(props) {
 
   }
 
+
   return (
-    <div className="register-container">
       <Form className="col-4 register-form">
         <Form.Group controlId="formBasicUsername">
           <Form.Label className="register-label">Username: </Form.Label>
-          <Form.Control type="text" value={username} onChange={e => createUsername(e.target.value)} placeholder="enter your username" />
+          <Form.Control type="text" value={username} className="username-input" onChange={e => createUsername(e.target.value)} placeholder="enter your username" />
         </Form.Group>
         <Form.Group controlId="formBasicPassword">
           <Form.Label className="register-label">Password</Form.Label>
-          <Form.Control type="password" value={password} onChange={e => createPassword(e.target.value)} placeholder="password" />
+          <Form.Control type="password" value={password} className="password-input" onChange={e => createPassword(e.target.value)} placeholder="password" />
         </Form.Group>
         <Form.Group controlId="formBasicEmail">
           <Form.Label className="register-label">Email address</Form.Label>
-          <Form.Control type="email" value={email} onChange={e => createEmail(e.target.value)} placeholder="email" />
+          <Form.Control type="email" value={email} className="email-input"  onChange={e => createEmail(e.target.value)} placeholder="email" />
         </Form.Group>
         <Form.Group controlId="exampleForm.ControlInput1">
           <Form.Label className="register-label">Date of Birth</Form.Label>
           <Form.Control type="date" value={birthday} onChange={e => createBirthday(e.target.value)} />
         </Form.Group>
         <div className="register-button-area">
-          <Button variant="secondary" type="button" className="register-button" onClick={() => props.onClick()}>Cancel</Button>
+          <Link to={'/'}>
+            <Button variant="secondary" type="button" className="register-button">Cancel</Button>
+          </Link>
           <Button variant="secondary" type="button" className="register-button" onClick={handleSubmit}>Submit</Button>
         </div>
       </Form>
-    </div>
-  );
+    );
 }
 
 
