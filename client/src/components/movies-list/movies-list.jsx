04650021d3ea6b285fc2cbe4896aa1fa2696d73d
lakkot <40659615+lakkot@users.noreply.github.com>
2020-03-02 12:32:35 +0100

    Create movies-list.jsx

---
+++ client/src/components/movies-list/movies-list.jsx
@@ -0,0 +1,25 @@
+// src/components/movies-list/movies-list.jsx
+import React from 'react';
+import { connect } from 'react-redux';
+
+import { MovieCard } from '../movie-card/movie-card';
+
+const mapStateToProps = state => {
+  const { visibilityFilter } = state;
+  return { visibilityFilter };
+};
+
+function MoviesList(props) {
+  const { movies, visibilityFilter } = props;
+  let filteredMovies = movies;
+
+  if (visibilityFilter !== '') {
+    filteredMovies = movies.filter(m => m.title.includes(visibilityFilter));
+  }
+
+  if (!movies) return <div className="main-view" />;
+
+  return filteredMovies.map(m => <MovieCard key={m._id} movie={m} />);
+}
+
+export default connect(mapStateToProps)(MoviesList);
